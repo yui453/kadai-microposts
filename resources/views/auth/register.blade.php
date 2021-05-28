6920d2cc4af0e9c5ec30391943c15ef370334d8b
yui <gakusyuu112@gmail.com>
2021-05-28 07:59:25 +0000

    user login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,19 +10,19 @@
             {!! Form::open(['route' => 'signup.post']) !!}
                 <div class="form-group">
                     {!! Form::label('name', 'Name') !!}
-                    {!! Form::text('name', null, ['class => 'form-control']) !!}
+                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group">
                     {!! Form::label('email', 'Email') !!}
-                    {!! Form::text('email', null, ['class => 'form-control']) !!}
+                    {!! Form::text('email', null, ['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group">
                     {!! Form::label('password', 'Password') !!}
-                    {!! Form::text('password', null, ['class => 'form-control']) !!}
+                    {!! Form::text('password', null, ['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group">
                     {!! Form::label('password_confirmation', 'Confirmation') !!}
-                    {!! Form::text('password_confirmation', null, ['class => 'form-control']) !!}
+                    {!! Form::text('password_confirmation', null, ['class' => 'form-control']) !!}
                 </div>
                 
                 {!! Form::submit('Sign up', ['class' => 'btn btn-primary btn-block']) !!}
