f9c683d1c5c88f6b50546580fce78da81bd77e2d
yui <gakusyuu112@gmail.com>
2021-06-05 04:39:10 +0000

    follow, unfollow

--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -7,14 +7,10 @@
             @include('users.card')
         </aside>
         <div class="col-sm-8">
-           {{-- タブ --}}
+            {{-- タブ --}}
             @include('users.navtabs')
-            @if (Auth::id() == $user->id)
-                {{-- 投稿フォーム --}}
-                @include('microposts.form')
-            @endif
-            {{-- 投稿一覧 --}}
-            @include('microposts.microposts')
+            {{-- ユーザ一覧 --}}
+            @include('users.users')
         </div>
     </div>
 @endsection
